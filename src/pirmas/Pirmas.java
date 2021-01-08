f2d8921455e7fdc8f743aaa794cd5cf4ef3d4257
beatarynkevic <beatainn@gmail.com>
2021-01-08 13:48:35 +0200

    Java code

---
+++ src/pirmas/Pirmas.java
@@ -0,0 +1,102 @@
+package pirmas;
+
+import java.util.Scanner;
+
+public class Pirmas {
+
+    public static void main(String[] args) {
+        
+        int suma = 0;
+        
+        for (int i = 15; i <= 37; i += 1 ) {
+            suma = suma + i;
+        }
+        System.out.println(suma);
+//        int i = 3;
+//        int j = 5;
+//        do {
+//            System.out.println(i); //spausdinam reiksme
+//            i= i + 1;
+//        } while (i < j);
+//        System.out.println("-----");
+//        i = 5;
+//        j = 3;
+//        while (i < j) {
+//            System.out.println(i);
+//            i = i + 1;
+//        }
+        
+        //alt+shift+l
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("nuo");
+//        int nuo = sc.nextInt();
+//        System.out.println("iki");
+//        int iki = sc.nextInt();
+//        int c = nuo;
+//        int d = iki - nuo + 1; //sito nereik
+//        //while(c<=iki){}
+//        while (nuo <= iki) {
+//            //int i = nuo;
+//            //while(i<=iki){}
+//            while (c<= iki) {
+//                System.out.print(c * nuo + " "); //i*nuo
+//                c++; //i++
+//        }
+//            System.out.println();
+//            nuo++; //c++
+//            c = c-d; //tada sito nereik
+//        }
+    }
+}
+        
+        
+//        System.out.println(); //numeta eilute
+//        Scanner sc = new Scanner(System.in);
+//        int sk = sc.nextInt();
+//        if(sk < 0) {
+//            System.out.println("neigiamas");
+//        } else if (sk == 0) {
+//            System.out.println("nulis");
+//        } else if (sk < 10) {
+//            System.out.println("mazas");
+//        }else if (sk < 100) {
+//            System.out.println("vidutinis");
+//        } else {
+//            System.out.println("didelis");
+//        }
+//        System.out.println("pabaiga");
+        
+//        Scanner sc = new Scanner(System.in);
+//        int sk = sc.nextInt();
+//        boolean b = sk > 0;
+//        if (b)  //jeigu cia reiksme true, tai vykdoma toliau
+//            System.out.println("teigiamas");
+//        
+//        
+//        System.out.println("pabaiga");
+
+        
+//        Scanner sc = new Scanner(System.in); //duomenu ivedimui   
+//        System.out.println("Ivesk 1 skaiciu:");
+//        int pirmas = sc.nextInt();  //specialus funkcionalumas
+//        System.out.println("Ivesk 2 skaiciu");
+//        double antras = sc.nextDouble();
+//        
+//        double suma = pirmas + antras;
+//  
+//        System.out.println("suma: " + suma);
+        
+//        int b = 100;
+//        
+//        int a = b = (2 + 8) / 4 * 2;
+//        
+//        float f = (2 + 8) / 4.0f * 2;
+//        
+//        int c = 10 / 4;
+//        
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(f);
+//    }
+//    
+//}
